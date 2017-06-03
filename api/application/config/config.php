08bc932419e64137e1c373dd2eafe512993151e9
vjsuthar1991 <suthar67@gmail.com>
2017-06-03 13:26:16 +0530

    setup project

--- api/application/config/config.php
+++ api/application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ei-asset/api/';
+$config['base_url'] = 'http://localhost/school-mgmt/api/';
 
 /*
 |--------------------------------------------------------------------------
